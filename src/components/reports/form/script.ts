a97afe72aa74253f75115d294b29a062926a5074
Kenta Yamada <knt.yamada.800@gmail.com>
2021-10-12 15:15:57 +0900

    implement value change event

--- src/components/reports/form/script.ts
+++ src/components/reports/form/script.ts
@@ -5,5 +5,34 @@ import Vue from "vue";
  */
 export default Vue.extend({
   name: "report-form",
+  methods: {
+    handleUpdatePracticeDate(value: Date): void {
+      this.$emit("update-practice-date", value);
+    },
+
+    handleUpdatePracticeTimeFrom(value: Date): void {
+      this.$emit("update-practice-time-from", value);
+    },
+
+    handleUpdatePracticeTimeTo(value: Date): void {
+      this.$emit("update-practice-time-to", value);
+    },
+
+    handleUpdateGoal(value: string): void {
+      this.$emit("update-goal", value);
+    },
+
+    handleUpdatePlan(value: string): void {
+      this.$emit("update-plan", value);
+    },
+
+    handleUpdateImplessions(value: string): void {
+      this.$emit("update-implessions", value);
+    },
+
+    handleUpdateNextAction(value: string): void {
+      this.$emit("update-next-action", value);
+    }
+  },
   template: "<report-form/>"
 });
