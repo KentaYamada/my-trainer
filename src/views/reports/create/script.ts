d291e844d34827701acc969bebbc92e490538f05
Kenta Yamada <knt.yamada.800@gmail.com>
2021-10-13 19:58:16 +0900

    implement save function

--- src/views/reports/create/script.ts
+++ src/views/reports/create/script.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import { mapActions, mapGetters } from "vuex";
+import { BNoticeConfig } from "buefy/types/components";
 import { required } from "vuelidate/lib/validators";
 import ReportForm from "@/components/reports/form/ReportForm.vue";
 import {
   INITIALIZE,
   REPORT,
+  SAVE,
   UPDATE_GOAL,
   UPDATE_IMPLESSIONS,
   UPDATE_MEMO,
@@ -14,6 +16,7 @@ import {
   UPDATE_PRACTICE_TIME_FROM,
   UPDATE_PRACTICE_TIME_TO
 } from "@/store/report/constant";
+import { FirestoreError } from "firebase/firestore";
 
 /**
  * Report create view
@@ -22,6 +25,11 @@ export default Vue.extend({
   name: "report-create-view",
   components: {
     ReportForm
+  },
+  data() {
+    return {
+      progressing: false
+    };
   },
   computed: {
     ...mapGetters("report", {
@@ -34,6 +42,7 @@ export default Vue.extend({
   methods: {
     ...mapActions("report", {
       initialize: INITIALIZE,
+      save: SAVE,
       updateGoal: UPDATE_GOAL,
       updateImplessions: UPDATE_IMPLESSIONS,
       updateMemo: UPDATE_MEMO,
@@ -46,6 +55,16 @@ export default Vue.extend({
 
     handleSave(): void {
       this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        this.progressing = true;
+        this.save(this.report)
+          .then(() => this._showToastSuccess("保存しました"))
+          .catch((error: FirestoreError) => this._showToastDanger(error.message))
+          .finally(() => (this.progressing = false));
+      } else {
+        this._showToastDanger("入力内容に誤りがあります。エラーメッセージを確認してください");
+      }
     },
 
     handleUpdatePracticeDate(value: Date): void {
@@ -78,6 +97,22 @@ export default Vue.extend({
 
     handleUpdateMemo(value: string): void {
       this.updateMemo(value);
+    },
+
+    _showToastDanger(message: string): void {
+      const config: BNoticeConfig = {
+        message: message,
+        type: "is-danger"
+      };
+      this.$buefy.toast.open(config);
+    },
+
+    _showToastSuccess(message: string): void {
+      const config: BNoticeConfig = {
+        message: message,
+        type: "is-success"
+      };
+      this.$buefy.toast.open(config);
     }
   },
   validations: {
