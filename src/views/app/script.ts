fdd00fd60a460980cd9fcdab542df5b94956ab81
Kenta Yamada <knt.yamada.800@gmail.com>
2021-10-24 12:43:35 +0900

    implement navigation visiblity control

--- src/views/app/script.ts
+++ src/views/app/script.ts
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Navigation from "@/components/navigations/Navigation.vue";
+import { mapGetters } from "vuex";
+import { IS_SIGN_IN } from "@/store/auth/constatnt";
 
 /**
  * App entry view
@@ -8,5 +10,10 @@ export default Vue.extend({
   name: "app-view",
   components: {
     Navigation
+  },
+  computed: {
+    ...mapGetters("auth", {
+      isSignIn: IS_SIGN_IN
+    })
   }
 });
