a8dcc96c78e7a4aceafdb469d5e55b7618654204
Kenta Yamada <knt.yamada.800@gmail.com>
2021-10-25 10:31:14 +0900

    implement format date function

--- src/filters/date-format.ts
+++ src/filters/date-format.ts
@@ -4,6 +4,10 @@ export const toYears = (payload: Date): string => {
   return moment(payload).format("YYYY年MM月");
 };
 
+export const toDate = (payload: Date): string => {
+  return moment(payload).format("YYYY年MM月DD日");
+};
+
 export const toTime = (payload: Date): string => {
   return moment(payload).format("HH:MM");
 };
